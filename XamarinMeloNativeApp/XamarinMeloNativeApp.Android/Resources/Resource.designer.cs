48678cf9d72d5e4424e165c9dc2acf6fcdb52b0e
VictorQrt <vquerette@gmail.com>
2017-05-21 10:06:32 +0200

    Manifest ; UI

--- XamarinMeloNativeApp/XamarinMeloNativeApp.Android/Resources/Resource.designer.cs
+++ XamarinMeloNativeApp/XamarinMeloNativeApp.Android/Resources/Resource.designer.cs
@@ -1940,8 +1940,8 @@ namespace XamarinMeloNativeApp.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060085
-			public const int action0 = 2131099781;
+			// aapt resource value: 0x7f060086
+			public const int action0 = 2131099782;
 			
 			// aapt resource value: 0x7f06005a
 			public const int action_bar = 2131099738;
@@ -1967,8 +1967,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f06005b
 			public const int action_context_bar = 2131099739;
 			
-			// aapt resource value: 0x7f060089
-			public const int action_divider = 2131099785;
+			// aapt resource value: 0x7f06008a
+			public const int action_divider = 2131099786;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -2015,8 +2015,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060072
 			public const int button_signin = 2131099762;
 			
-			// aapt resource value: 0x7f060086
-			public const int cancel_action = 2131099782;
+			// aapt resource value: 0x7f060087
+			public const int cancel_action = 2131099783;
 			
 			// aapt resource value: 0x7f06002b
 			public const int center = 2131099691;
@@ -2030,8 +2030,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060052
 			public const int checkbox = 2131099730;
 			
-			// aapt resource value: 0x7f06008c
-			public const int chronometer = 2131099788;
+			// aapt resource value: 0x7f06008d
+			public const int chronometer = 2131099789;
 			
 			// aapt resource value: 0x7f060033
 			public const int clip_horizontal = 2131099699;
@@ -2090,8 +2090,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f06001c
 			public const int end = 2131099676;
 			
-			// aapt resource value: 0x7f060091
-			public const int end_padder = 2131099793;
+			// aapt resource value: 0x7f060092
+			public const int end_padder = 2131099794;
 			
 			// aapt resource value: 0x7f060023
 			public const int enterAlways = 2131099683;
@@ -2135,8 +2135,11 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f06003f
 			public const int image = 2131099711;
 			
-			// aapt resource value: 0x7f060090
-			public const int info = 2131099792;
+			// aapt resource value: 0x7f060085
+			public const int imageView1 = 2131099781;
+			
+			// aapt resource value: 0x7f060091
+			public const int info = 2131099793;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
@@ -2144,11 +2147,11 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f06002f
 			public const int left = 2131099695;
 			
-			// aapt resource value: 0x7f06008a
-			public const int line1 = 2131099786;
+			// aapt resource value: 0x7f06008b
+			public const int line1 = 2131099787;
 			
-			// aapt resource value: 0x7f06008e
-			public const int line3 = 2131099790;
+			// aapt resource value: 0x7f06008f
+			public const int line3 = 2131099791;
 			
 			// aapt resource value: 0x7f06000b
 			public const int listMode = 2131099659;
@@ -2159,11 +2162,11 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060069
 			public const int main_content = 2131099753;
 			
-			// aapt resource value: 0x7f060088
-			public const int media_actions = 2131099784;
+			// aapt resource value: 0x7f060089
+			public const int media_actions = 2131099785;
 			
-			// aapt resource value: 0x7f060093
-			public const int menu_edit = 2131099795;
+			// aapt resource value: 0x7f060094
+			public const int menu_edit = 2131099796;
 			
 			// aapt resource value: 0x7f06001d
 			public const int middle = 2131099677;
@@ -2285,8 +2288,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060013
 			public const int showTitle = 2131099667;
 			
-			// aapt resource value: 0x7f060092
-			public const int sliding_tabs = 2131099794;
+			// aapt resource value: 0x7f060093
+			public const int sliding_tabs = 2131099795;
 			
 			// aapt resource value: 0x7f06007a
 			public const int snackbar_action = 2131099770;
@@ -2315,8 +2318,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060031
 			public const int start = 2131099697;
 			
-			// aapt resource value: 0x7f060087
-			public const int status_bar_latest_event_content = 2131099783;
+			// aapt resource value: 0x7f060088
+			public const int status_bar_latest_event_content = 2131099784;
 			
 			// aapt resource value: 0x7f060065
 			public const int submit_area = 2131099749;
@@ -2327,11 +2330,11 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060075
 			public const int tabs = 2131099765;
 			
-			// aapt resource value: 0x7f06008f
-			public const int text = 2131099791;
+			// aapt resource value: 0x7f060090
+			public const int text = 2131099792;
 			
-			// aapt resource value: 0x7f06008d
-			public const int text2 = 2131099789;
+			// aapt resource value: 0x7f06008e
+			public const int text2 = 2131099790;
 			
 			// aapt resource value: 0x7f06004d
 			public const int textSpacerNoButtons = 2131099725;
@@ -2339,8 +2342,8 @@ namespace XamarinMeloNativeApp.Droid
 			// aapt resource value: 0x7f060080
 			public const int textView1 = 2131099776;
 			
-			// aapt resource value: 0x7f06008b
-			public const int time = 2131099787;
+			// aapt resource value: 0x7f06008c
+			public const int time = 2131099788;
 			
 			// aapt resource value: 0x7f060043
 			public const int title = 2131099715;
